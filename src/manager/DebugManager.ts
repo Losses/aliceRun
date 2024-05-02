a0e8b0f934feb7a91aaf76c4a069179908d08709
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-05-02 17:29:00 +0800

    chore: Put it into the real context

--- src/manager/DebugManager.ts
+++ src/manager/DebugManager.ts
@@ -6,8 +6,8 @@ import { timeManager } from './TimeManager';
 import { FrameRateLevel } from '../utils/TimeMagic';
 
 export const DebugManager = (camera: THREE.Camera, scene: THREE.Scene, tracker: ResourceTracker) => {
-    camera.position.set(-1200, 0, 0);
-    camera.lookAt(60, 0, 0);
+    // camera.position.set(-1200, 0, 0);
+    // camera.lookAt(60, 0, 0);
 
     const controls = new OrbitControls(camera, document.querySelector("#app") as HTMLDivElement);
     controls.update();
