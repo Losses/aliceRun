93231df9c305a40483d8e33db491494e5e0efba6
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-05-02 13:29:46 +0800

    chore: stop shining!

--- src/components/shaders/grassFragment.glsl
+++ src/components/shaders/grassFragment.glsl
@@ -1,6 +1,5 @@
 precision highp float;
 
-uniform float time;
 in vec3 vPosition;
 in vec4 vColor;
 
@@ -8,7 +7,6 @@ out vec4 fragColor;
 
 void main() {
     vec4 color = vec4(vColor);
-    color.r += sin(vPosition.x * 10.0 + time) * 0.5;
 
     fragColor = color;
 }
