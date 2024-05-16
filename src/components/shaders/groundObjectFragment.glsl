4f5632a7ae9e3c4e1d3afcd89f25d0bd8bb5ed79
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-05-16 18:42:29 +0800

    chore: Code clean up

--- src/components/shaders/groundObjectFragment.glsl
+++ src/components/shaders/groundObjectFragment.glsl
@@ -54,13 +54,9 @@ float snoise(vec3 v) {
    vec3 i1 = min(g.xyz, l.zxy);
    vec3 i2 = max(g.xyz, l.zxy);
 
-  //   x0 = x0 - 0.0 + 0.0 * C.xxx;
-  //   x1 = x0 - i1  + 1.0 * C.xxx;
-  //   x2 = x0 - i2  + 2.0 * C.xxx;
-  //   x3 = x0 - 1.0 + 3.0 * C.xxx;
    vec3 x1 = x0 - i1 + C.xxx;
-   vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
-   vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
+   vec3 x2 = x0 - i2 + C.yyy;
+   vec3 x3 = x0 - D.yyy;
 
 // Permutations
    i = mod289(i);
@@ -83,8 +79,6 @@ float snoise(vec3 v) {
    vec4 b0 = vec4(x.xy, y.xy);
    vec4 b1 = vec4(x.zw, y.zw);
 
-  //vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;
-  //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
    vec4 s0 = floor(b0) * 2.0 + 1.0;
    vec4 s1 = floor(b1) * 2.0 + 1.0;
    vec4 sh = -step(h, vec4(0.0));
