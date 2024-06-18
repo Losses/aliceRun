3825bac814ba44e8e47f0a99564b014f2f63289c
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-06-18 21:54:35 +0800

    chore: Move spector to debug manager

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// @ts-ignore
-import { Spector } from 'spectorjs';
 import { initializeThreeCanvas } from './initializeThreeCanvas';
 import { AudioManager } from './manager/AudioManager';
 import { ColorManager } from './manager/ColorManager';
@@ -50,8 +48,6 @@ GroundObjectManager(
 window.requestAnimationFrame(() => {
    timeManager.addFn(() => config.composer.render(), FrameRateLevel.D0);
    timeManager.play();
-   const spector = new Spector();
-   spector.displayUI();
 });
 
 JoyConManager();
--- src/manager/DebugManager.ts
+++ src/manager/DebugManager.ts
@@ -1,7 +1,8 @@
 // biome-ignore lint/style/useImportType: We need to use it as non-type while debugging.
 import * as THREE from 'three'; 
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
+// @ts-ignore
+// import { Spector } from 'spectorjs';
 import type { ResourceTracker } from '../utils/ResourceTracker';
 // import { timeManager } from './TimeManager';
 // import { FrameRateLevel } from '../utils/TimeMagic';
@@ -25,6 +26,10 @@ export const DebugManager = (
    // timeManager.addFn(() => {
    //     controls.update();
    // }, FrameRateLevel.D0);
+
+   
+   // const spector = new Spector();
+   // spector.displayUI();
 
    return {};
 };
