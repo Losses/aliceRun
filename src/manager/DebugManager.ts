9750dcdd6f6eaecc5390f7906d0f0724ca5981bf
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-06-19 20:05:40 +0800

    chore: Code formatting

--- src/manager/DebugManager.ts
+++ src/manager/DebugManager.ts
@@ -1,5 +1,5 @@
 // biome-ignore lint/style/useImportType: We need to use it as non-type while debugging.
-import * as THREE from 'three'; 
+import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // @ts-ignore
 import { Spector } from 'spectorjs';
@@ -16,20 +16,19 @@ export const DebugManager = (
    if (enable) {
       camera.position.set(-1200, 0, 0);
       camera.lookAt(60, 0, 0);
-   
+
       const controls = new OrbitControls(camera, document.querySelector("#app") as HTMLDivElement);
       controls.update();
-   
+
       const axesHelper = new THREE.AxesHelper(5);
       scene.add(axesHelper);
-   
+
       tracker.track(controls);
-   
+
       timeManager.addFn(() => {
-          controls.update();
+         controls.update();
       }, FrameRateLevel.D0);
-   
-      
+
       const spector = new Spector();
       spector.displayUI();
    }
