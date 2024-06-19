e5fa3dcdb49881135fcab3d19eff2751d5c85147
LOSSES Don <1384036+Losses@users.noreply.github.com>
2024-06-19 19:44:52 +0800

    feat: Better debug manager

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ StoryManager();
 RunStatManager();
 StoryListManager();
 HpManager(config.effects);
-DebugManager(config.camera, config.scene, config.tracker);
+DebugManager(config.camera, config.scene, config.tracker, true);
 GroundManager(config.camera, config.scene, config.tracker);
 GrassManager(config.camera, config.scene, config.tracker, config.renderer);
 SkyBoxManager(config.camera, config.scene, config.tracker);
--- src/manager/DebugManager.ts
+++ src/manager/DebugManager.ts
@@ -1,35 +1,38 @@
 // biome-ignore lint/style/useImportType: We need to use it as non-type while debugging.
 import * as THREE from 'three'; 
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // @ts-ignore
-// import { Spector } from 'spectorjs';
+import { Spector } from 'spectorjs';
 import type { ResourceTracker } from '../utils/ResourceTracker';
-// import { timeManager } from './TimeManager';
-// import { FrameRateLevel } from '../utils/TimeMagic';
+import { timeManager } from './TimeManager';
+import { FrameRateLevel } from '../utils/TimeMagic';
 
 export const DebugManager = (
    camera: THREE.Camera,
    scene: THREE.Scene,
    tracker: ResourceTracker,
+   enable = false,
 ) => {
-   // camera.position.set(-1200, 0, 0);
-   // camera.lookAt(60, 0, 0);
-
-   // const controls = new OrbitControls(camera, document.querySelector("#app") as HTMLDivElement);
-   // controls.update();
-
-   // const axesHelper = new THREE.AxesHelper(5);
-   // scene.add(axesHelper);
-
-   // tracker.track(controls);
-
-   // timeManager.addFn(() => {
-   //     controls.update();
-   // }, FrameRateLevel.D0);
-
+   if (enable) {
+      camera.position.set(-1200, 0, 0);
+      camera.lookAt(60, 0, 0);
    
-   // const spector = new Spector();
-   // spector.displayUI();
+      const controls = new OrbitControls(camera, document.querySelector("#app") as HTMLDivElement);
+      controls.update();
+   
+      const axesHelper = new THREE.AxesHelper(5);
+      scene.add(axesHelper);
+   
+      tracker.track(controls);
+   
+      timeManager.addFn(() => {
+          controls.update();
+      }, FrameRateLevel.D0);
+   
+      
+      const spector = new Spector();
+      spector.displayUI();
+   }
 
    return {};
 };
